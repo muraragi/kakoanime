61eca5eaafba82b43e1da949c97b53fd3361c010
Marat Yuzbashev <muraragi99@gmail.com>
2023-02-06 00:49:39 +0300

    some styling for the soul

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { goto } from '$app/navigation'
 	import type { PageData } from './$types'
 
 	export let data: PageData
@@ -10,12 +11,28 @@
 
 <h2 class="mb-4 text-xl">User likes:</h2>
 <div class="grid grid-cols-5 gap-3">
+	<div
+		class="align-center flex cursor-pointer items-center justify-center rounded-2xl border border-dashed border-stone-300 text-6xl transition-colors hover:bg-zinc-800"
+		on:click={() => goto('/explore')}
+		on:keypress={(event) => {
+			if (event.key === 'Enter') goto('/explore')
+		}}
+		role="button"
+		tabindex="0"
+	>
+		+
+	</div>
 	{#if data.likes.length}
 		{#each data.likes as like}
-			<div class="break-words">
-				{like.title}
-				{like.jikanId}
-				<img src={like.poster} alt={like.title} />
+			<div class="group relative cursor-pointer">
+				<img src={like.poster} alt={like.title} class="max-h-80 w-full rounded-2xl object-cover" />
+				<div
+					class="absolute left-0 top-0 flex h-full w-full items-center justify-center rounded-2xl opacity-0 backdrop-blur-lg transition-opacity group-hover:opacity-100"
+				>
+					<div class="anime-title px-4 text-center text-xl font-semibold">
+						{like.title}
+					</div>
+				</div>
 			</div>
 		{/each}
 	{:else}
@@ -24,3 +41,9 @@
 </div>
 
 <p class="mt-6">This is from env: {data.fromEnv}</p>
+
+<style>
+	.anime-title {
+		text-shadow: 2px 3px 2px rgba(0, 0, 0, 0.3);
+	}
+</style>
